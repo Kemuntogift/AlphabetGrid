faf098882e66c0a63368e067038a06eec741e8e6
Kemunto Gift <giftoyugi@gmail.com>
2022-05-25 13:59:56 +0300

    generate methods from the android widget adapter

--- app/src/main/java/com/kemunto/alphabetgrid/AlphabetAdapter.java
+++ app/src/main/java/com/kemunto/alphabetgrid/AlphabetAdapter.java
@@ -1,4 +1,36 @@
 package com.kemunto.alphabetgrid;
 
-public class AlphabetAdapter extends BaseAdapter{
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AlphabetAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mLetters;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    public AlphabetAdapter (Context context, String[] letters){
+        this.mContext = context;
+        this.mLetters = letters;
+    }
 }
